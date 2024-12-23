99df2c1babd23f09bbb243af56fdf118070fe2cb
zhouxiang <1092171397@qq.com>
2024-12-23 18:00:42 +0800

    修改两个文件

--- Gnu-socket-client.py
+++ Gnu-socket-client.py
@@ -2,16 +2,20 @@
 # -*- coding: UTF-8 -*-
 
 import socket
-
-s = socket.socket()
-host = socket.gethostname()
-port = 12345
-s.bind((host,port))
-
-s.listen(5)
+# 创建socket对象
+socket_client = socket.socket()
+# 连接到服务器
+socket_client.connect(("localhost", 8888))
+ 
 while True:
-    c,addr = s.accept()
-    print ('链接地址：' , addr)
-    c.send('hello')
-    c.close()
-    
+    send_msg = input("请输入要发送给服务端的消息：")
+    if send_msg == "exit":
+        break
+    # 发送消息
+    socket_client.send(send_msg.encode("UTF-8"))
+    # 接受消息
+    recv_data = socket_client.recv(1024).decode("UTF-8")    # 1024是缓冲区大小，一般就填1024， recv是阻塞式
+    print(f"服务端回复的消息是：{recv_data}")
+ 
+# 关闭连接
+socket_client.close()
--- Gnu-socket-server.py
+++ Gnu-socket-server.py
@@ -3,10 +3,39 @@
 
 import socket
 
-s = socket.socket()
-host = socket.gethostname()
+#创建socket对象
+socket_server = socket.socket()
+
+#利用gethostname()函数获取主机地址
+host = "localhost"
+#设定一个端口号
 port = 12345
 
-s.connet((host,port))
-print (s.recv(1024))
-s.close()
+#绑定IP和端口号
+socket_server.connect((host,port))
+
+#listen方法接收一个整数传参数，表示接受的连接数量，可不填
+socket_server.listen()
+
+#等待客户端连接，accept方法返回二元元组(连接对象, 客户端地址信息)
+# accept()方法是阻塞式的方法，如果没有客户端连接，会一直等待，不往下执行
+print("服务器已开始监听，正在等待客户端连接...")
+connect_name,address = socket_server.accept()
+
+print("接收到了客户端的连接，客户端的信息：{address}")
+
+#接收客户端信息，使用客户端和服务端的本次连接对象，而非socket_server
+while True:
+    #接收消息
+    data: str = connect_name.recv(1024).decode("UTF-8")
+    print("客户端发来的的消息：{data}")
+
+    #回复消息
+    msg = input("请输入你要回复客户端的信息")
+    if msg == 'exit':
+        break
+    connect_name.send(msg.encode("UTF-8"))
+
+#关闭连接
+connect_name.close()
+socket_server.close()
