97de02168600b60fd8affe55bd32038dfcb81fb8
zhouxiang <1092171397@qq.com>
2024-12-24 17:59:58 +0800

    修改两个socket为UDP通信，成功

--- Gnu-socket-client.py
+++ Gnu-socket-client.py
@@ -2,20 +2,27 @@
 # -*- coding: UTF-8 -*-
 
 import socket
+
 # 创建socket对象
-socket_client = socket.socket()
+socket_client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # 连接到服务器
-socket_client.connect(("localhost", 8888))
- 
+socket_client.bind(('127.0.0.1',8081))
+
 while True:
     send_msg = input("请输入要发送给服务端的消息：")
-    if send_msg == "exit":
-        break
-    # 发送消息
-    socket_client.send(send_msg.encode("UTF-8"))
-    # 接受消息
+    socket_client.sendto(send_msg.encode("UTF-8"),('127.0.0.1',8888))
     recv_data = socket_client.recv(1024).decode("UTF-8")    # 1024是缓冲区大小，一般就填1024， recv是阻塞式
     print(f"服务端回复的消息是：{recv_data}")
+    
+# socket_client.connect(('127.0.0.1', 8888))
  
-# 关闭连接
-socket_client.close()
+# while True:
+#     send_msg = input("请输入要发送给服务端的消息：")
+#     if send_msg == "exit":
+#         break
+#     # 发送消息
+#     socket_client.send(send_msg.encode("UTF-8"))
+#     # 接受消息
+ 
+# # 关闭连接
+# socket_client.close()
--- Gnu-socket-server.py
+++ Gnu-socket-server.py
@@ -4,38 +4,43 @@
 import socket
 
 #创建socket对象
-socket_server = socket.socket()
+socket_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-#利用gethostname()函数获取主机地址
-host = "localhost"
 #设定一个端口号
-port = 12345
+port = 8888
 
 #绑定IP和端口号
-socket_server.connect((host,port))
+socket_server.bind(('127.0.0.1',port))
+
+while True:
+    data = socket_server.recvfrom(1024)[0].decode("UTF-8")
+    print(f"客户端发来的消息为 {data}")
+    send_msg = input("请输入要发送给该客户端的消息：")
+    socket_server.sendto(send_msg.encode("UTF-8"),('127.0.0.1',8081))
 
 #listen方法接收一个整数传参数，表示接受的连接数量，可不填
-socket_server.listen()
 
-#等待客户端连接，accept方法返回二元元组(连接对象, 客户端地址信息)
-# accept()方法是阻塞式的方法，如果没有客户端连接，会一直等待，不往下执行
-print("服务器已开始监听，正在等待客户端连接...")
-connect_name,address = socket_server.accept()
+# socket_server.listen()
 
-print("接收到了客户端的连接，客户端的信息：{address}")
+# #等待客户端连接，accept方法返回二元元组(连接对象, 客户端地址信息)
+# # accept()方法是阻塞式的方法，如果没有客户端连接，会一直等待，不往下执行
+# print("服务器已开始监听，正在等待客户端连接...")
+# conn,address = socket_server.accept()
 
-#接收客户端信息，使用客户端和服务端的本次连接对象，而非socket_server
-while True:
-    #接收消息
-    data: str = connect_name.recv(1024).decode("UTF-8")
-    print("客户端发来的的消息：{data}")
+# print(f"接收到了客户端的连接，客户端的信息：{address}")
 
-    #回复消息
-    msg = input("请输入你要回复客户端的信息")
-    if msg == 'exit':
-        break
-    connect_name.send(msg.encode("UTF-8"))
+# #接收客户端信息，使用客户端和服务端的本次连接对象，而非socket_server
+# while True:
+#     #接收消息
+#     data: str = conn.recv(1024).decode("UTF-8")
+#     print("客户端发来的的消息：{}".format(data))
 
-#关闭连接
-connect_name.close()
-socket_server.close()
+#     #回复消息
+#     msg = input("请输入你要回复客户端的信息")
+#     if msg == 'exit':
+#         break
+#     conn.send(msg.encode("UTF-8"))
+
+# #关闭连接
+# conn.close()
+# socket_server.close()
