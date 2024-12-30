bc5c057a764dfb28470aa9e01eee869277607a72
zhouxiang <1092171397@qq.com>
2024-12-30 20:13:14 +0800

    利用socketPDU实现了socket向GNURadio传输内容

--- Gnu-socket-server.py
+++ Gnu-socket-server.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 import socket
+import time
 
 #创建socket对象
 socket_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -10,37 +11,27 @@ socket_server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = 8888
 
 #绑定IP和端口号
-socket_server.bind(('127.0.0.1',port))
+socket_server.bind(('192.168.26.1',port))
+print("UDP服务已经启动，等待发送消息")
 
-while True:
-    data = socket_server.recvfrom(1024)[0].decode("UTF-8")
-    print(f"客户端发来的消息为 {data}")
-    send_msg = input("请输入要发送给该客户端的消息：")
-    socket_server.sendto(send_msg.encode("UTF-8"),('127.0.0.1',8081))
+# 预定义消息列表
+messages = ["hello",  "\n","zhouxiang!", "How are you?", "55555", "Test message"]
 
-#listen方法接收一个整数传参数，表示接受的连接数量，可不填
+print("UDP 服务已启动，开始自动发送消息...")
 
-# socket_server.listen()
+try:
+    while True:
+        for msg in messages:
+            # 发送消息
+            socket_server.sendto(msg.encode("UTF-8"), ('192.168.26.134', 8081))
+            print(f"已发送消息：{msg}")
+            time.sleep(0.1)  # 每次发送间隔时间
+except KeyboardInterrupt:
+    print("程序已手动终止。")
 
-# #等待客户端连接，accept方法返回二元元组(连接对象, 客户端地址信息)
-# # accept()方法是阻塞式的方法，如果没有客户端连接，会一直等待，不往下执行
-# print("服务器已开始监听，正在等待客户端连接...")
-# conn,address = socket_server.accept()
 
-# print(f"接收到了客户端的连接，客户端的信息：{address}")
-
-# #接收客户端信息，使用客户端和服务端的本次连接对象，而非socket_server
 # while True:
-#     #接收消息
-#     data: str = conn.recv(1024).decode("UTF-8")
-#     print("客户端发来的的消息：{}".format(data))
-
-#     #回复消息
-#     msg = input("请输入你要回复客户端的信息")
-#     if msg == 'exit':
-#         break
-#     conn.send(msg.encode("UTF-8"))
-
-# #关闭连接
-# conn.close()
-# socket_server.close()
+#     # data = socket_server.recvfrom(1024)[0].decode("UTF-8")
+#     # print(f"客户端发来的消息为 {data}")
+#     send_msg = input("请输入要发送给该客户端的消息：")
+#     socket_server.sendto(send_msg.encode("UTF-8"),('192.168.26.134',8081))
